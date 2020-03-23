a622cad8e608094b29cd09626144e06a88c6d43c
zejaz <zayn.ejaz@gmail.com>
2020-03-23 20:47:51 +0300

    added testimonial section with JS responiveness

--- js/script.js
+++ js/script.js
@@ -2,6 +2,27 @@
     'use strict';
 
     $(document).on('ready', function () {
+        // ----------------TESTIMONIAL SLIDER (OWL)----------------
+        $('.testimonial-slider').owlCarousel({
+            autoplay: true,
+            loop: true,
+            margin: 20,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                320: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                992: {
+                    items: 2
+                }
+            }
+        });
         
         
         // ----------------HAMBURGER ANIMATION----------------
