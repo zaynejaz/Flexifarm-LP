daf7b846b4c88d9e05c33fb1781f4b017368306c
zejaz <zayn.ejaz@gmail.com>
2020-03-23 21:06:11 +0300

    added navbar selector + smooth scrolling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
     'use strict';
 
     $(document).on('ready', function () {
-        // ----------------TESTIMONIAL SLIDER (OWL)----------------
+
+        // ===================TESTIMONIAL SLIDER (OWL)===================
         $('.testimonial-slider').owlCarousel({
             autoplay: true,
             loop: true,
@@ -24,21 +25,63 @@
             }
         });
         
+        // ===================NAVBAR NAVIGATION SELECTOR + SMOOTH SCROLL + HAMBURGER ANIMATION===================
         
-        // ----------------HAMBURGER ANIMATION----------------
-        
+        //cache selectors
+        var lastId,
+            topMenu = $("#navbarNav"),
+            topMenuHeight = topMenu.outerHeight()-1,
+            //list items (all)
+            menuItems = topMenu.find("a"),
+            //corresponding to menu item anchors
+            scrollItems = menuItems.map(function(){
+              var item = $($(this).attr("href"));
+              if (item.length) { return item; }
+            });
+        // click handler bind to menu items
+        //smooth scroll animation
+        menuItems.click(function(e){
+          var href = $(this).attr("href"),
+              offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+          $('html, body').stop().animate({ 
+              scrollTop: offsetTop
+          }, 600);
+          e.preventDefault();
+        });
+
+        //scroll binding
+        $(window).scroll(function(){
+           //container scroll position
+           var fromTop = $(this).scrollTop()+topMenuHeight;
+           //remove current scroll item id
+           var cur = scrollItems.map(function(){
+             if ($(this).offset().top < fromTop)
+               return this;
+           });
+           //current element id
+           cur = cur[cur.length-1];
+           var id = cur && cur.length ? cur[0].id : "";
+
+           if (lastId !== id) {
+               lastId = id;
+               //set/remove active class
+               menuItems
+                 .parent().removeClass("active")
+                 .end().filter("[href='#"+id+"']").parent().addClass("active");
+           }                   
+        });
 
         // hamburger to X on close
         $("#hamburger-menu").click( function(){
             $(".icon").toggleClass("close");
         })
         
-        // ----------------ANIMATE ON SCROLL (AOS)----------------
+        // ===================ANIMATE ON SCROLL (AOS)===================
         AOS.init({
             once: false
         });
 
-        // ----------------AUTO UPDATE CURRENT YEAR----------------
+        // ===================AUTO UPDATE CURRENT YEAR===================
         window.addEventListener("load", ()=> {
             !function() {
                 var e=new Date;
